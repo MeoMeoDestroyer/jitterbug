e9eb59ba513450ee2236b6bfb463d09e54185cf9
Kate_nguyen <253353253+MeoMeoDestroyer@users.noreply.github.com>
2026-01-27 20:45:06 +0000

    add worm

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,51 @@
 #include <bn_core.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
+#include <bn_random.h>
+#include <bn_keypad.h>
+#include <bn_vector.h>
+#include <bn_fixed_point.h>
 #include <bn_sprite_ptr.h>
 
-#include "bn_sprite_items_bug.h"
+#include "bn_sprite_items_worm.h"
 
-int main() {
+int main()
+{
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(31, 0, 31));
+    bn::random rng;
 
-    bn::sprite_ptr ladybug = bn::sprite_items::bug.create_sprite();
+    bn::backdrop::set_color(bn::color(31, 9, 24));
 
-    while(true) {
+    bn::vector<bn::sprite_ptr, 16> worms;
+
+    while(true)
+    {
+        // Move existing worms randomly
+        for(bn::sprite_ptr& worm : worms)
+        {
+            bn::fixed new_x = worm.x() + rng.get_fixed(-1, 1);   // [-1, 1)
+            bn::fixed new_y = worm.y() + rng.get_fixed(-1, 1);   // [-1, 1)
+
+            worm.set_x(new_x);
+            worm.set_y(new_y);
+        }
+
+        // Spawn a new worm when A pressed
+        if(bn::keypad::a_pressed())
+        {
+            if(worms.full())
+            {
+                // optional: do nothing or overwrite an old one
+            }
+            else
+            {
+                // Give the new worm a starting position (center of screen)
+                bn::sprite_ptr new_worm = bn::sprite_items::worm.create_sprite(0, 0);
+                worms.push_back(bn::move(new_worm));
+            }
+        }
+
         bn::core::update();
     }
 }
